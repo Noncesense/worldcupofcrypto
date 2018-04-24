9562263058647ceb626cb6e92f7f72f6f235366f
Sebastiaan van Kuyk <sebastiaan.vankuyk@gmail.com>
2018-04-24 14:07:23 -0500

    safemath

--- predictionfactory.sol
+++ predictionfactory.sol
@@ -1,27 +1,32 @@
 pragma solidity ^0.4.19;
 
 import "./ownable.sol";
+import "./safemath.sol";
 
 contract PredictionFactory is Ownable {
 
-  event NewPrediction(uint predictionId, uint dna);
+  using SafeMath for uint256;
+  using SafeMath32 for uint32;
+  using SafeMath16 for uint16;
+
+  event NewPrediction(uint predictionId, string dna);
 
   uint predictionFee = 0.001 ether;
 
   struct Prediction {
-    uint dna;
+    string dna;
   }
 
   Prediction[] public predictions;
 
-  mapping(uint => address) public predictionToOwner;
+  mapping(string => address) public predictionToOwner;
   mapping(address => uint) ownerPredictionCount;
 
-  function createPrediction(uint _dna) external payable {
+  function createPrediction(string _dna) external payable {
     require(msg.value == predictionFee);
     uint id = predictions.push(Prediction(_dna)) -1;
     predictionToOwner[id] = msg.sender;
-    ownerpredictionCount[msg.sender]++;
+    ownerpredictionCount[msg.sender] = ownerpredictionCount[msg.sender].add(1);
     NewPrediction(id, _dna);
   }
 
--- predictionhelper.sol
+++ predictionhelper.sol
@@ -4,8 +4,14 @@ import "./predictionfactory.sol";
 
 contract PredictionHelper is PredictionFactory {
 
+  string finalResult = "";
+
   function withdraw() external onlyOwner {
     owner.transfer(0.1 * this.balance);
+  }
+
+  function setFinalResult(string _dna) external onlyOwner {
+    finalResult = _dna;
   }
 
 }
